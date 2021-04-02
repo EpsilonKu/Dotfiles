151fdd8b6eb272fb9e541068b6fbdfa34ca85257
DumbEpsKu <63652620+EpsilonKu@users.noreply.github.com>
2021-04-02 13:19:51 +0600

    Neovim: Added required plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,39 +1,76 @@
 
 vim.cmd [[packadd packer.nvim]]
--- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
--- vim._update_package_paths()
 
 vim.cmd [[ autocmd BufWritePost plugins.lua PackerCompile ]]
 
 return require('packer').startup(function()
-  -- Packer can manage itself as an optional plugin
-    use {'wbthomason/packer.nvim', opt = true}
-    use {'blackcauldron7/surround.nvim'}
-    use {
-		"numtostr/gruvbox-material",
+	use {'wbthomason/packer.nvim', opt = true}
+	use {'blackcauldron7/surround.nvim'}
+	use {
+		"savq/melange",
 		config = require("plugins._theme").config
 	} -- Colorscheme
-    use {'vim-airline/vim-airline'}
-    use {
+	use {
+		'datwaft/bubbly.nvim',
+		config = require("plugins._bubbly").config
+	}
+	use {
 		"mhinz/vim-startify",
 		config = require("plugins._startify").config
 	}
-    use {
+	use {
 		"voldikss/vim-floaterm",
 		config = require("plugins._floaterm").config
 	}
-    use {'preservim/nerdtree'}
-    use {'psliwka/vim-smoothie'}
-	use {'luochen1990/rainbow'}
 	use "jiangmiao/auto-pairs"
 	use {
-                    "neoclide/coc.nvim",
-                    branch = "release",
-                    config = require("plugins._coc").config
-    }
+		"neoclide/coc.nvim",
+		branch = "release",
+		config = require("plugins._coc").config
+	}
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {
+			{
+				'nvim-lua/popup.nvim'
+			}, 
+			{
+				'nvim-lua/plenary.nvim'
+			}
+		}
+	}
+	use {
+		"oberblastmeister/neuron.nvim"
+	}
+	use {
+		'kyazdani42/nvim-web-devicons'
+	}
+	use {
+		'romgrk/barbar.nvim',
+		config = require("plugins._barbar").config
+	}
+	use {
+		'kyazdani42/nvim-tree.lua',
+		config = require("plugins._tree").config
+	}
+	use {
+		'karb94/neoscroll.nvim'
+	}
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		run = {
+			':TSUpdate',
+			':TSInstall cpp'
+		},
+		config = require("plugins._treesitter").config
+	}
+	use {
+		'p00f/nvim-ts-rainbow'
+	}
     -- Plug 'wincent/command-t'
-    use {'liuchengxu/vim-which-key'}
-    use {'tibabit/vim-templates'}
+--    use {'liuchengxu/vim-which-key'}
+--    use {'tibabit/vim-templates'}
+--	use {'npxbr/glow.nvim' ,'do': ':GlowInstall'}
     --  use {'wellle/context.vim'}
     -- Plug 'jakobkogler/Algorithm-DataStructures'
     -- Plug 'prabirshrestha/vim-lsp'
@@ -50,7 +87,7 @@ return require('packer').startup(function()
     -- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
 
     -- Load on an autocommand event
-    use {'andymass/vim-matchup', event = 'VimEnter *'}
+ --    use {'andymass/vim-matchup', event = 'VimEnter *'}
 
     -- Load on a combination of conditions: specific filetypes or commands
     -- Also run code after load (see the "config" key)
