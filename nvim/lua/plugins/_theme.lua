88b25c03cdfe9b2f9032db2881fbb3dea9f65471
DumbEpsKu <63652620+EpsilonKu@users.noreply.github.com>
2021-04-02 13:19:11 +0600

    Neovim: Updated theme to "Melange"

--- nvim/lua/plugins/_theme.lua
+++ nvim/lua/plugins/_theme.lua
@@ -1,13 +1,8 @@
 local M = {}
 
 function M.config()
-	 local g = vim.g
-	 g.gruvbox_material_background = "hard"
-    g.gruvbox_material_sign_column_background = "none"
-    -- g.gruvbox_material_transparent_background = 1
-    g.gruvbox_material_better_performance = 1
-
-    vim.api.nvim_command("colorscheme gruvbox-material")
+	local g = vim.g
+	vim.api.nvim_command("colorscheme melange")
 end
 
 return M
